bfabb4c56fbd40bc702a7c635b38a465c611944b
Rahmatullo <qosimovrahmatullo006@gmail.com>
2026-06-27 17:34:57 +0500

    feat: category resource wrong import namespace

--- app/Filament/Admin/Resources/CategoryResource.php
+++ app/Filament/Admin/Resources/CategoryResource.php
@@ -13,6 +13,7 @@ use Filament\Actions\EditAction;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Resources\Resource;
+use Filament\Schemas\Components\Grid;
 use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 use Filament\Tables;
